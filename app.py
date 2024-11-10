ebbf30d26cb7909ce00d602d53a9a7a6256b6a96
ia935 <imranhahmed0987@gmail.com>
2024-11-09 20:31:23 -0500

    Making index.html for our website

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request, url_for
+import sqlite3
 
 app = Flask(__name__)
 #homepage route
 @app.route("/")
 #connecting to database
+
+
+def get_db_connection():
+    conn = sqlite3.connect('flashcards.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 def index():
-    return "Welcome to the Flashcard App!"
+    conn = get_db_connection()
+    flashcards = conn.execute('select * from flashcards').fetchall()
+    conn.close()
+    #homepage template
+    return render_template('index.html',flashcards=flashcards)
 
 
 
