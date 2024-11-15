908e1089b8e0d243fe9843a1474c303839f401ea
Imran Ahmed <imranhahmed0987@gmail.com>
2024-11-14 20:24:04 -0500

    adding formatting

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,8 @@
 function confirmDelete() {
     return confirm("Are you sure you want to delete this flashcard?");
 }
+function toggleAnswer(flashcardElement) {
+    const answerElement = flashcardElement.querySelector('.answer');
+    // Toggle the display of the answer
+    answerElement.style.display = (answerElement.style.display === "none") ? "block" : "none";
+}
